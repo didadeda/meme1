b627c5b5ebdb2c7b09ebe1412b4c20195a07b92e
digitalveins <alfredo1.reyes1@gmail.com>
2020-05-13 23:12:02 +0200

    setterfunction to button and submitinputs

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React, { useEffect, useState } from 'react';
 import "./app.css";
 
-const submitInputs = () => {
-  const firstInput = document.getElementById('firstInput').value 
-  const secondInput = document.getElementById('secondInput').value 
+const submitInputs = (setMemeText) => {
+  const firstInput = document.getElementById('firstInput').value; 
+  const secondInput = document.getElementById('secondInput').value; 
 };
 
 export default () => {
   const [memes, setMemes] = useState([]);
   const [currentMeme, setCurrentMeme] = useState();
+  const [memeText, setMemeText] = useState({
+    firstText: "",
+    secondText: "";
+  });
   useEffect(() => {
     fetch("https://api.imgflip.com/get_memes")
     .then((result) => result.json())
@@ -23,7 +27,7 @@ return (
   <div>
     <input id="firstInput" placeholder="First Text" type="text" />
     <input id="secondInput" placeholder="Second Text" type="text" />
-    <button onClick={(e) => submitInputs}>Generate Meme Text</button>
+    <button onClick={(e) => submitInputs(setMemeText)}>Generate Meme Text</button>
     {currentMeme ? (
       <div id="memeContainer">
         <img id="memeImage" src={currentMeme.url} /> 
